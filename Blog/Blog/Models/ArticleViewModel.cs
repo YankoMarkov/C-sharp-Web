ac5e9de4be1a40847eddec4e3be8f0eac42e2241
Yanko Markov <yanko@markov.eu>
2017-08-27 12:28:05 +0300

    Add files via upload

---
+++ Blog/Blog/Models/ArticleViewModel.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Web;
+
+namespace Blog.Models
+{
+    public class ArticleViewModel
+    {
+        public int Id { get; set; }
+
+        [Require]
+        [StringLength(50)]
+        public string Title { get; set; }
+
+        [Require]
+        public string Content { get; set; }
+
+        public string AuthorId { get; set; }
+    }
+}
