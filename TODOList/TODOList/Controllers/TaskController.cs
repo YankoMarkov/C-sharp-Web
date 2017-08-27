332aa0a0ba89c382e4fa49d268b2db8b59fffa3c
Yanko Markov <yanko@markov.eu>
2017-08-27 12:19:18 +0300

    Add files via upload

---
+++ TODOList/TODOList/Controllers/TaskController.cs
@@ -0,0 +1,44 @@
+﻿
+using System.Web.Mvc;
+using TODOList.Models;
+
+namespace TODOList.Controllers
+{
+    public class TaskController : Controller
+    {
+        [HttpPost]
+        public ActionResult Create(Task task)
+        {
+            if (task == null)
+            {
+                return RedirectToAction("Index", "Home");
+            }
+            using (var db = new TaskDbContext())
+            {
+                db.Task.Add(task);
+                db.SaveChanges();
+            }
+            return RedirectToAction("Index", "Home");
+        }
+
+        [HttpGet]
+        public ActionResult Delete(int? id)
+        {
+            if (id == null)
+            {
+                return RedirectToAction("Index", "Home");
+            }
+            using (var db = new TaskDbContext())
+            {
+                var task = db.Task.Find(id);
+                if (task == null)
+                {
+                    return RedirectToAction("Index", "Home");
+                }
+                db.Task.Remove(task);
+                db.SaveChanges();
+            }
+            return RedirectToAction("Index", "Home");
+        }
+    }
+}
