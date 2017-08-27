cb66ed3751990dea4c148d921fa29b825c52714b
Yanko Markov <yanko@markov.eu>
2017-08-27 12:23:59 +0300

    Add files via upload

---
+++ Calculator/Calculator-CSharp/Models/Calculator.cs
@@ -0,0 +1,17 @@
+﻿
+
+namespace Calculator_CSharp.Models
+{
+    public class Calculator
+    {
+        public decimal LeftOperand { get; set; }
+        public decimal RightOperand { get; set; }
+        public string Operator { get; set; }
+        public decimal Result { get; set; }
+
+        public Calculator()
+        {
+            this.Result = 0;
+        }
+    }
+}
